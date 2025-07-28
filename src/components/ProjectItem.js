68bde0163f509d4e1932e3c6e1afc5cc892d0f1b
iconic-coder <mbatiasir@gmail.com>
2025-07-28 18:54:55 +0300

    passed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,13 +3,17 @@ import React from "react";
 function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
-      <h3>{name}</h3>
+      <h2>{name}</h2>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech) => (
+          <span key={tech}>{tech}</span>
+        ))}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
+
